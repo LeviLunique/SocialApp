396909cee3422ed20d2433ae9e258c9bba2fe559
Levi Lunique <levi.lunique@gmail.com>
2021-09-02 18:45:22 -0400

    Initial Commit

---
+++ SocialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialApp
+//
+//  Created by user204006 on 9/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialApp/SocialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAppApp.swift
+//  SocialApp
+//
+//  Created by user204006 on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SocialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
